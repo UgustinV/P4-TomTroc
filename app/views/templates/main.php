b31a48105fc08cc0aefd9b6561d0db4559bae45f
Augustin Viard <gugusviard@gmail.com>
2025-12-12 18:27:23 +0100

    feat/layout responsive

--- app/views/templates/main.php
+++ app/views/templates/main.php
@@ -18,9 +18,30 @@
                 <a href="/P4-TomTroc/public/">Accueil</a>
                 <a href="/P4-TomTroc/public/books">Nos livres à l'échange</a>
             </div>
+            <input type="checkbox" id="menu-toggle" class="menu-toggle">
+            <label for="menu-toggle" class="hamburger">
+                <img src="/P4-TomTroc/public/images/burger_menu.svg" alt="Menu" class="burger-icon">
+            </label>
+            <div id="burger-content">
+                <ul>
+                    <li>
+                        <a href="/P4-TomTroc/public/">Accueil</a>
+                    </li>
+                    <li>
+                        <a href="/P4-TomTroc/public/books">Nos livres à l'échange</a>
+                    </li>
+                    <?php 
+                        if (isset($_SESSION['user'])) {
+                            echo '<li><a href="index.php?action=contact">Messagerie</a></li><li><a href="index.php?action=faq">Mon Compte</a></li><li><a href="index.php?action=disconnectUser">Déconnexion</a></li>';
+                        }
+                        else {
+                            echo '<li><a href="/P4-TomTroc/public/login">Connexion</a></li>';
+                        }
+                        ?>
+                </ul>
+            </div>
             <div id="account-manager">
                 <?php 
-                    // Si on est connecté, on affiche le bouton de déconnexion, sinon, on affiche le bouton de connexion : 
                     if (isset($_SESSION['user'])) {
                         echo '<a href="index.php?action=contact">Messagerie</a><a href="index.php?action=faq">Mon Compte</a><a href="index.php?action=disconnectUser">Déconnexion</a>';
                     }
