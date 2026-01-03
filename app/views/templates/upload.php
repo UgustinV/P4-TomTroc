087fc3427d1b131fb2b48dbb90426cdf0e6db7d7
Augustin Viard <gugusviard@gmail.com>
2026-01-03 19:12:51 +0100

    feat/upload done

--- app/views/templates/upload.php
+++ app/views/templates/upload.php
@@ -1,37 +1,43 @@
 <div id="upload-page">
-    <div id="upload-form">
-        <h1>Ajouter un livre</h1>
-        <form action="/P4-TomTroc/public/upload" method="POST" enctype="multipart/form-data">
-            <?php if (isset($errors) && !empty($errors)): ?>
-                <div class="errors">
-                    <?php foreach ($errors as $error): ?>
-                        <p><?= htmlspecialchars($error) ?></p>
-                    <?php endforeach; ?>
-                </div>
-            <?php endif; ?>
-            <div id="form-fields">
-                <div>
-                    <label for="title">Titre</label>
-                    <input type="text" id="title" name="title" required>
-                </div>
-                <div>
-                    <label for="writer">Auteur</label>
-                    <input type="text" id="writer" name="writer" required>
-                </div>
-                <div>
-                    <label for="description">Commentaire</label>
-                    <textarea id="description" name="description" required></textarea>
-                </div>
-                <div>
-                    <label for="availability">Disponibilité</label>
-                    <input type="checkbox" id="availability" name="is_available" value="1" checked>
-                </div>
-                <button type="submit">Ajouter</button>
+    <h1>Ajouter un livre</h1>
+    <form action="/P4-TomTroc/public/upload" method="POST" enctype="multipart/form-data">
+        <?php if (isset($errors) && !empty($errors)): ?>
+            <div class="errors">
+                <?php foreach ($errors as $error): ?>
+                    <p><?= htmlspecialchars($error) ?></p>
+                <?php endforeach; ?>
             </div>
+        <?php endif; ?>
+        <div id="form-fields">
+            <div>
+                <label for="title">Titre</label>
+                <input class="input" type="text" id="title" name="title" required>
+            </div>
+            <div>
+                <label for="writer">Auteur</label>
+                <input class="input" type="text" id="writer" name="writer" required>
+            </div>
+            <div>
+                <label for="description">Commentaire</label>
+                <textarea id="description" name="description" required></textarea>
+            </div>
+            <div>
+                <label for="availability">Disponibilité</label>
+                <select class="input" id="availability" name="is_available" value="1">
+                    <option value="1" selected>Disponible</option>
+                    <option value="0">Indisponible</option>
+                </select>
+            </div>
+            <button type="submit">Ajouter</button>
+        </div>
+        <div id="image-input">
+            <p>Photo</p>
             <div id="image-field">
-                <img src="/P4-TomTroc/public/images/upload_illustration.svg" alt="Upload Illustration">
-                <input type="file" id="image" name="image" accept="image/*" required></input>
+                <label for="image-import">
+                    <img src="/P4-TomTroc/public/images/upload_illustration.svg" alt="Upload Illustration">
+                </label>
+                <input type="file" id="image-import" name="image" accept=".png, .jpg, .jpeg" required>
             </div>
-        </form>
-    </div>
+        </div>
+    </form>
 </div>
